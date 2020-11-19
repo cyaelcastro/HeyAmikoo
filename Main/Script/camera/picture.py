0500208bcd77530fcfd901400652145071674dff
Hochob <cyaelcc94@gmail.com>
2020-11-18 23:16:29 -0600

    Add eog handling with python subprocess for Amikoo camera script
    Even when the eog subprocess was tested with photo files, needs
    a deeper review in the way the picture.py is called considering
    the python version

--- Main/Script/camera/picture.py
+++ Main/Script/camera/picture.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import shutil
 import time
+import subprocess
 
 def main():
   os.system("killall -9 eog")
@@ -10,11 +11,14 @@ def main():
   name = str(name)+".jpg"
   os.system("fswebcam -r 800x480 --jpeg 80 --no-banner --save " + name)
   os.system("mv "+name+" /home/lupe/Desktop/photos/"+name)
-  #shutil.move(name,"/home/lupe/Desktop/photos/"+name)
-  #time.sleep(1)
-  os.system("eog -f /home/lupe/Desktop/photos/"+name+" &")
-  time.sleep(3)
-  os.system("killall -9 eog")
+  shutil.move(name,"/home/lupe/Desktop/photos/"+name)
+  time.sleep(1)
+    
+  try:
+    eog_subprocess = subprocess.run(["eog", "-f", "/home/hochob/HeyAmikoo/Image/"+name], timeout=3)
+  except subprocess.TimeoutExpired:
+    print("EOG Closed")
+
   return 0  
 
 if __name__ == '__main__':
