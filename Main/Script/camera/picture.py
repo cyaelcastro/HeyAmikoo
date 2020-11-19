e03a04f710c9048d2b453b59bdb47cf4d22265e2
Hochob <cyaelcc94@gmail.com>
2020-11-18 23:39:29 -0600

    Modularize picture python script
    Cleaning the code adding functions to improve readability

--- Main/Script/camera/picture.py
+++ Main/Script/camera/picture.py
@@ -4,22 +4,38 @@ import shutil
 import time
 import subprocess
 
-def main():
-  os.system("killall -9 eog")
+
+def photo_name():
   now = datetime.datetime.now()
   name = str(now.year)+"-"+str(now.month)+"-"+str(now.day)+"-"+str(now.hour)+"-"+str(now.minute)+"-"+str(now.second)
   name = str(name)+".jpg"
-  os.system("fswebcam -r 800x480 --jpeg 80 --no-banner --save " + name)
-  os.system("mv "+name+" /home/lupe/Desktop/photos/"+name)
-  shutil.move(name,"/home/lupe/Desktop/photos/"+name)
-  time.sleep(1)
-    
+  return name
+
+
+def take_picture(file_name):
+  os.system("fswebcam -r 800x480 --jpeg 80 --no-banner --save " + file_name)
+  os.system("mv "+file_name+" /home/lupe/Desktop/photos/"+file_name)
+  shutil.move(file_name,"/home/lupe/Desktop/photos/"+file_name)
+  return "/home/lupe/Desktop/photos/"+file_name
+
+
+def show_picture(file_location):
   try:
-    eog_subprocess = subprocess.run(["eog", "-f", "/home/hochob/HeyAmikoo/Image/"+name], timeout=3)
+    eog_subprocess = subprocess.run(["eog", "-f", file_location], timeout=3)
   except subprocess.TimeoutExpired:
     print("EOG Closed")
 
+
+def main():
+
+  os.system("killall -9 eog")
+  
+  file_name = photo_name()
+  file_location = take_picture(file_name)
+  show_picture(file_location)
+    
   return 0  
+
 
 if __name__ == '__main__':
  main()
